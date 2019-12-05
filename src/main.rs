470b327d13301020546c8e7725bfbc0a073966ae
Joshua Davey <josh@joshuadavey.com>
2019-12-05 10:19:43 -0500

    Add basic command line options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use]
 extern crate bitflags;
 extern crate nom;
+#[macro_use]
+extern crate clap;
 
 mod font;
 mod settings;
 mod text;
 
+use clap::{App, Arg};
 use font::{read_font, Font};
 use settings::Settings;
 use text::Text;
@@ -56,11 +59,42 @@ fn art_lines(message: &str, font: &Font, settings: &Settings, max_width: usize) 
 }
 
 fn main() {
+    let app = App::new("rustlet")
+        .version(crate_version!())
+        .about("ASCII art from messages")
+        .arg(Arg::with_name("width")
+             .short("w")
+             .long("width")
+             .value_name("WIDTH")
+             .default_value("72")
+             .help("Sets the maximum width for a line")
+             .takes_value(true))
+        // .arg(Arg::with_name("font")
+        //      .short("f")
+        //      .long("font")
+        //      .value_name("FONT")
+        //      .default_value("standard")
+        //      .help("Name of font file to use")
+        //      .takes_value(true))
+        .arg(Arg::with_name("messages")
+             .multiple(true));
+
+    let matches = app.get_matches();
+
     let rawfont = include_str!("../fonts/small.flf");
     let font = read_font(rawfont).unwrap();
 
-    let message = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
-    let max_size: usize = 70;
+    let max_size: usize = matches
+        .value_of("width")
+        .unwrap()
+        .parse()
+        .expect("Choose a valid positive number for width");
+
+    let message = matches
+        .values_of("messages")
+        .expect("Provide some text to format")
+        .collect::<Vec<_>>()
+        .join(" ");
 
     let art_lines = art_lines(&message, &font, &font.settings, max_size);
 
